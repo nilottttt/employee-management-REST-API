61358762aa743ee08e9ba26b90f3daf92fc4eb20
nilottttttt <nilotpalarya65@gmail.com>
2026-07-01 22:30:34 +0530

    feat: implement pagination, sorting, and dynamic filtering for employee endpoints

--- src/main/java/ems_backend/service/impl/EmployeeServiceImpl.java
+++ src/main/java/ems_backend/service/impl/EmployeeServiceImpl.java
@@ -5,9 +5,16 @@ import lombok.AllArgsConstructor;
 import ems_backend.dto.EmployeeDto;
 import ems_backend.service.EmployeeService;
 import ems_backend.repository.EmployeeRepository;
+import ems_backend.dto.EmployeeResponse;
 import ems_backend.entity.Employee;
 import ems_backend.mapper.EmployeeMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -65,5 +72,36 @@ public class EmployeeServiceImpl implements EmployeeService {
         );
 
         employeeRepository.deleteById(employeeId);
+    }
+
+    @Override
+    public EmployeeResponse getAllEmployees(int pageNo, int pageSize, String sortBy, String sortDir) {
+
+        //Configure the Sort object dynamically based on the direction requested
+        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ?
+                Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
+
+        //Create the Pageable instance
+        Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
+
+        //Fetch the "Page" from the database.
+        Page<Employee> employees = employeeRepository.findAll(pageable);
+
+        //Extract the raw Entity data and map it to DTOs
+        List<Employee> listOfEmployees = employees.getContent();
+        List<EmployeeDto> content = listOfEmployees.stream()
+                .map(EmployeeMapper::mapToEmployeeDto)
+                .collect(Collectors.toList());
+
+        //Build the EmployeeResponse object with the data AND the metadata
+        EmployeeResponse employeeResponse = new EmployeeResponse();
+        employeeResponse.setContent(content);
+        employeeResponse.setPageNo(employees.getNumber());
+        employeeResponse.setPageSize(employees.getSize());
+        employeeResponse.setTotalElements(employees.getTotalElements());
+        employeeResponse.setTotalPages(employees.getTotalPages());
+        employeeResponse.setLast(employees.isLast());
+
+        return employeeResponse;
     }
 }
